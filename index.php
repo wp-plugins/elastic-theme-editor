e838b4cd5e5a81c477fbd320394ae75491a433a4
koopersmith <koopersmith@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-20 19:15:01 +0000

    retagging 0.0.2.8
    git-svn-id: https://plugins.svn.wordpress.org/elastic-theme-editor/trunk@185405 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: Elastic Theme Editor
 Plugin URI: http://wordpress.org/extend/plugins/elastic-theme-editor/
 Description: A theme editor for the Elastic framework.
-Version: 0.0.2.7
+Version: 0.0.2.8
 Author: Daryl Koopersmith
 Author URI: http://gsoc2009wp.wordpress.com/tag/elastic/
 
